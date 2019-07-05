a4ed65ca2e055757a059d4135e9c79a99225d950
Rahamathullah S <rahamath18@yahoo.com>
2019-07-05 16:46:07 +0530

    Initial commit

---
+++ C-Sharp-WebSocketClient/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using WebSocketSharp;
+
+namespace C_Sharp_WebSocketClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WebSocket wsClient = new WebSocket("ws://localhost:8080");
+            wsClient.Connect();
+            wsClient.Send("Hi I am websocket client.");
+            
+            wsClient.OnOpen += (sender, e) =>
+            {
+                Console.WriteLine("OnOpen : server msg : " + e);
+            };
+
+            wsClient.OnMessage += (sender, e) =>
+            {
+                Console.WriteLine("New Message Received From Server : " + e.Data);
+            };
+
+            wsClient.OnClose += (sender, e) =>
+            {
+                Console.WriteLine("OnClose : server msg : " + e);
+            };
+
+            wsClient.OnError += (sender, e) =>
+            {
+                Console.WriteLine("OnError : server msg : " + e);
+            };
+
+            wsClient.Send("Bye from websocket client.");
+            Console.ReadKey(true);
+           
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,6 @@
+
+C# Websocket Client
+-------------------
+
+C# client lib : install 'WebSocketSharp-netstandard 1.0.1'
+
